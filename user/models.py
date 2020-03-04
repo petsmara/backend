c3f815efbc19bb739216e2fa68f0f9ed2d11d2ec
MarieJung <marie@MarieJungui-MacBookPro.local>
2020-03-05 04:47:17 +0900

    Implement class user in user/models.py
    1. create 'class user' in user/models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email        = models.EmailField(max_length      = 300, unique = True)
+    password     = models.CharField(max_length       = 300)
+    nickname     = models.CharField(max_length       = 300)
+    phone_number = models.CharField(max_length       = 300)
+    created_at   = models.DateTimeField(auto_now_add = True)
+    updated_at   = models.DateTimeField(auto_now     = True)
+
+    class Meta:
+        db_table = 'users'
+
