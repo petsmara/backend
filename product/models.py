9ec0df138efb50efc2860c3e8abb3ba180612d09
MarieJung <marie@MarieJungui-MacBookPro.local>
2020-03-08 16:41:44 +0900

    Fix typo in ./product/models.py
    1. fix 'class user' into 'from user'
    2. fix content's text field option 'max_length = 5000' to None

--- product/models.py
+++ product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class user.models import User
+from  user.models import User
 
 class ProductCategory(models.Model):
     category_name = models.CharField(max_length = 500)
@@ -11,7 +11,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     seller_id   = models.ForeignKey(User, on_delete            = models.CASCADE, null = True)
     title       = models.CharField(max_length                  = 500)
-    content     = models.TextField(max_legnth                  = 5000)
+    content     = models.TextField()
     deal_spot   = models.CharField(max_length                  = 1000)
     category_id = models.ForeignKey(ProductCategory, on_delete = models.CASCADE)
     created_at  = models.DateTimeField(auto_now_add            = True)
