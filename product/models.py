7d463cbbe4b597ae571ea183ed95d2a3803bdf55
MarieJung <muchogusto.lol@gmail.com>
2020-03-09 19:48:01 +0900

    Add 'price' field into class Product
    1. add 'price' field into class Product of ./product/models.py

--- product/models.py
+++ product/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     seller_id   = models.ForeignKey(User, on_delete            = models.CASCADE, null = True)
     title       = models.CharField(max_length                  = 500)
     content     = models.TextField()
+    price       = models.DecimalField(max_digits=11, decimal_places=2)
     deal_spot   = models.CharField(max_length                  = 1000)
     category_id = models.ForeignKey(ProductCategory, on_delete = models.CASCADE)
     created_at  = models.DateTimeField(auto_now_add            = True)
