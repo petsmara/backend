1f73346bb33d06e0974767ed6658758ccbf581f5
MarieJung <muchogusto.lol@gmail.com>
2020-03-16 04:06:15 +0900

    Create 'image' app
    1. create 'image' app
    2. add 'image' into INSATALLED_APPS in settings.py
    3. add 'image' into ./petsmara/urls.py
    4. create ./image/urls.py

--- petsmara/urls.py
+++ petsmara/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user', include('user.urls')),
     path('product', include('product.urls')),
+    path('image',include('image.urls')),
 ]
