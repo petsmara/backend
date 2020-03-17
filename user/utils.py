43051bc82a67c5088b35a69d8e26c0d5a3cd0212
MarieJung <muchogusto.lol@gmail.com>
2020-03-17 22:32:53 +0900

    Fix login_decorator

---
+++ user/utils.py
@@ -0,0 +1,25 @@
+import jwt
+import json
+
+from django.http            import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from my_settings import SECRET_KEY
+from .models     import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
+            request.user = User.objects.get(id=payload["id"])
+            return func(self, request, *args, **kwargs)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status = 400)
+        except KeyError:
+            return JsonRespose({'message':'INVALID_KEY'}, status = 400)
+
+    return wrapper
+
+
