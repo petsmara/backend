295a9c62f3846f32a9b0673615e5ab1b9caf0368
MarieJung <muchogusto.lol@gmail.com>
2020-03-19 14:53:53 +0900

    Fix fields' name of product/models.py
    1. fix category_name into title
    2. fix seller_id into seller
    3. fix category_id into category

--- product/models.py
+++ product/models.py
@@ -3,20 +3,20 @@ from django.db import models
 from  user.models import User
 
 class ProductCategory(models.Model):
-    category_name = models.CharField(max_length = 500)
+    title = models.CharField(max_length = 500)
 
     class Meta:
         db_table = 'product_categories'
 
 class Product(models.Model):
-    seller_id   = models.ForeignKey(User, on_delete            = models.CASCADE, null = True)
-    title       = models.CharField(max_length                  = 500)
+    seller      = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
+    title       = models.CharField(max_length = 500)
     content     = models.TextField()
-    price       = models.DecimalField(max_digits=11, decimal_places=2)
-    places      = models.CharField(max_length                  = 1000)
-    category_id = models.ForeignKey(ProductCategory, on_delete = models.CASCADE)
-    created_at  = models.DateTimeField(auto_now_add            = True)
-    modified_at = models.DateTimeField(auto_now                = True)
+    price       = models.DecimalField(max_digits = 11, decimal_places = 2)
+    places      = models.CharField(max_length = 500, null = True)
+    category    = models.ForeignKey(ProductCategory, on_delete = models.CASCADE, null = True)
+    created_at  = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now = True)
 
     class Meta:
         db_table = 'products'
