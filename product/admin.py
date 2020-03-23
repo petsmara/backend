64f06dfda192714dbc660049dc04144f7feeca4a
MarieJung <muchogusto.lol@gmail.com>
2020-03-23 13:48:06 +0900

    Add product app into django admin
    1. fix ./product/admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product, ProductAdmin)
+
+
