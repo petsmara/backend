999973bc6d365f3c11cbebe75678ec0af77d2fcb
MarieJung <muchogusto.lol@gmail.com>
2020-03-30 19:11:51 +0900

    Implement UserProfileView
    1. create UserProfileView
    2. add UserProfileView into ./user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls    import path
 
-from .views import UserView, AuthView
+from .views import UserView, AuthView, UserProfile
 
 urlpatterns = [
    # path('admin/', admin.site.urls),
     path('/account/signup', UserView.as_view()),
-    path('/account/login', AuthView.as_view())
+    path('/account/login', AuthView.as_view()),
+    path('/account/profile', UserProfile.as_view()),
 ]
