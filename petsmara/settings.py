35d72f26dbd8dfa3ac0786a14a74f0fb1aa4c8f9
EunkyoungJung <muchogusto.lol@gmail.com>
2020-04-18 22:53:46 +0900

    Fix database setting

--- petsmara/settings.py
+++ petsmara/settings.py
@@ -72,13 +72,15 @@ WSGI_APPLICATION = 'petsmara.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+DATABASES = my_settings.DATABASES
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 
 # Password validation
