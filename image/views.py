d878191454525d20ffd070645cf03dea04f0d8d6
Ubuntu <ubuntu@ip-172-31-44-41.ap-northeast-2.compute.internal>
2020-04-22 13:49:04 +0000

    Limit max image size

--- image/views.py
+++ image/views.py
@@ -5,6 +5,8 @@ import time
 
 from django.views import View
 from django.http  import JsonResponse, HttpResponse
+from PIL          import Image
+from io           import BytesIO
 
 from user.utils   import login_decorator
 from aws_settings import S3_URL, S3_BUCKET_NAME, AWS_SECRET_KEY, AWS_ACCESS_SECRET_KEY
@@ -26,6 +28,35 @@ class ImageView(View):
         new_filename = '-'.join(list(map(lambda info:str(info), infos)))
         return new_filename
 
+    def _resize_file(self, file):
+        max_width, max_height = 500, 500
+
+        img = Image.open(file)
+        src_width, src_height = img.size
+        src_ratio = float(src_width) / float(src_height)
+        dst_width, dst_height = max_width, max_height
+        dst_ratio = float(dst_width) / float(dst_height)
+ 
+        if dst_ratio < src_ratio:
+            crop_height = src_height
+            crop_width  = crop_height * dst_ratio
+            x_offset    = int(src_width - crop_width) // 2
+            y_offset    = 0
+        else:
+            crop_width = src_width
+            crop_height = crop_width / dst_ratio
+            x_offset = 0
+            y_offset = int(src_height - crop_height) // 3
+        
+        img = img.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
+        img = img.resize((dst_width, dst_height), Image.ANTIALIAS)
+
+        image_file = BytesIO()
+        img.save(image_file, 'JPEG')
+        image_file.seek(0)
+        # data.file = image_file
+        return image_file
+
     @login_decorator
     def post(self, request):
         limit = int(request.POST.get('limit')) if request.POST.get('limit') else 5
@@ -41,8 +72,9 @@ class ImageView(View):
                 if img_index > limit:
                     break
                 new_filename = self._new_filename(request.user.id, img_index)
+                resized_image = self._resize_file(file)
                 self.s3_client.upload_fileobj(
-                    file,
+                    resized_image,
                     S3_BUCKET_NAME,
                     new_filename,
                     ExtraArgs={
